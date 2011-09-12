132ce1d60d6f6f636b54a5249bb42c55624d4dbd
Ross Mellgren <rmm-github@z.odi.ac>
2011-09-11 22:41:00 -0400

    VMs actually run to completion now

--- agent/src/main/scala/rwars2012/agent/ChildVM.scala
+++ agent/src/main/scala/rwars2012/agent/ChildVM.scala
@@ -1,15 +1,12 @@
 package rwars2012.agent
 
-import java.io.{File, StringWriter}
-import java.nio.{ByteBuffer, CharBuffer}
-import java.nio.channels.{Channels, ReadableByteChannel, SelectableChannel, Selector}
-import java.nio.charset.Charset
+import java.io.{BufferedReader, File, InputStream, InputStreamReader, StringWriter}
 import java.util.concurrent.{BrokenBarrierException, CyclicBarrier, Semaphore}
 import com.sun.jdi.{Bootstrap, ThreadReference, VirtualMachine}
 import com.sun.jdi.connect.Connector.{BooleanArgument, StringArgument}
 import com.sun.jdi.request.{StepRequest, ThreadDeathRequest, ThreadStartRequest}
 import net.lag.configgy.Configgy
-import net.lag.logging.Logger
+import org.slf4j.{Logger, LoggerFactory}
 import scala.collection.JavaConverters.{mapAsScalaMapConverter, asScalaSetConverter}
 
 object ProcessStream extends Enumeration {
@@ -18,13 +15,33 @@ object ProcessStream extends Enumeration {
     val STDERR = Value("stderr")
 }
 
-class ChildVMSupervisor {
-    val log = Logger(getClass)
+class ChildVMSupervisor(val quantum: Long) {
+    val log = LoggerFactory.getLogger(getClass)
 
-    private var barrier: CyclicBarrier = null
+    private var barrier: CyclicBarrier = new CyclicBarrier(1)
     private var childVMs: Map[Int, ChildVM] = Map.empty
     private var nextId: Int = 0
-    protected[agent] val selector: Selector = Selector.open()
+
+    private val supervisorThread = new Thread("ChildVM Supervisor " + System.identityHashCode(this)) {
+        setDaemon(true)
+
+        override def run() =
+            while (true) {
+                val b = synchronized(barrier)
+                try {
+                    b.await()
+                } catch {
+                    case (_: BrokenBarrierException) => {
+                        log.debug("barrier broken")
+                        run()
+                    }
+                }
+
+                log.debug("barrier converged")
+            }
+    }
+
+    supervisorThread.start()
 
     protected[agent] def converge(): Unit = {
         val b = synchronized(barrier)
@@ -35,31 +52,9 @@ class ChildVMSupervisor {
         }
     }
 
-    private val thread = new Thread(toString + " Thread") {
-        override def run(): Unit = {
-            selector.select(0)
-            val iter = selector.selectedKeys.iterator
-            while (iter.hasNext) {
-                val key = iter.next()
-                val (id, which) = key.attachment.asInstanceOf[(Int, ProcessStream.Value)]
-
-                childVMs.get(id) match {
-                    case Some(childVM) if key.isReadable => childVM.acceptInput(which, key.channel)
-                    case None if key.isReadable => log.debug("Ignoring input for child #" + id)
-                    case _ => {}
-                }
-
-                iter.remove()
-            }
-
-            run()
-        }
-    }
-    thread.start()
-
-    private def rebuildBarrier(newSize: Int): Unit = {
-        if (barrier != null) barrier.reset()
-        barrier = new CyclicBarrier(newSize)
+    private def rebuildBarrier(newChildCount: Int): Unit = {
+        barrier.reset()
+        barrier = new CyclicBarrier(newChildCount + 1)
     }
 
     def add(label: String, parameters: VMParameters): ChildVM =
@@ -76,7 +71,7 @@ class ChildVMSupervisor {
             if (childVMs contains childVM.id) {
                 rebuildBarrier(childVMs.size - 1)
                 childVMs -= childVM.id
-                childVM.unregister()
+                childVM.kill()
             }
         }
 }
@@ -84,7 +79,7 @@ class ChildVMSupervisor {
 case class VMParameters(targetMainClass: String, targetJARs: Set[String], cpuShare: Int, maxHeap: Long, maxStack: Int)
 
 class ChildVM protected[agent] (supervisor: ChildVMSupervisor, val id: Int, val label: String, val parameters: VMParameters) {
-    val log = Logger(getClass)
+    val log = LoggerFactory.getLogger(getClass.getName + "#" + id + "-" + label)
 
     val virtualMachine: VirtualMachine = {
         val agentJARPath = Configgy.config("agentJARPath")
@@ -94,183 +89,170 @@ class ChildVM protected[agent] (supervisor: ChildVMSupervisor, val id: Int, val 
         val args = arguments.asScala
         args("suspend").asInstanceOf[BooleanArgument].setValue(true)
         args("options").asInstanceOf[StringArgument].setValue (
-            "-classpath " + (parameters.targetJARs + agentJARPath).mkString(File.pathSeparator) +
-            "-Xmx" + parameters.maxHeap +
-            "-Xms" + parameters.maxStack
+            List (
+                "-classpath " + (parameters.targetJARs + agentJARPath).mkString(File.pathSeparator),
+                "-Xmx" + parameters.maxHeap,
+                "-Xss" + parameters.maxStack
+            ).mkString(" ")
         )
-        args("main").asInstanceOf[StringArgument].setValue("rwars2012.agent.childVM.Rig")
+        args("main").asInstanceOf[StringArgument].setValue("rwars2012.agent.rig.Rig")
+        log.info("Launching VM with arguments:")
+        args.values.foreach(arg => log.info("    " + arg.toString))
+        log.info("and parameters: " + parameters)
         connector.launch(arguments)
     }
 
-    val (stdoutSelectorKey, stderrSelectorKey) =
-        (Channels.newChannel(virtualMachine.process.getInputStream).register(supervisor.selector, SelectionKey.OP_READ, (id, ProcessStream.STDOUT)),
-         Channels.newChannel(virtualMachine.process.getErrorStream).register(supervisor.selector, SelectionKey.OP_READ, (id, ProcessStream.STDERR)));
-
-    log.info("#" + id + " " + label + ": Registered.")
-
-    protected[agent] def unregister(): Unit = {
-        stdoutSelectorKey.cancel()
-        stderrSelectorKey.cancel()
+    protected[agent] def kill(): Unit = {
         virtualMachine.exit(0)
-        log.info("#" + id + " " + label + ": Unregistered.")
+        log.info("Killed.")
     }
 
+    private val stdoutStreamMonitor =
+        new StreamLogger("stdout", virtualMachine.process.getInputStream, s => log.info("stdout: " + s))
+    private val stderrStreamMonitor =
+        new StreamLogger("stderr", virtualMachine.process.getErrorStream, s => log.error("stderr: " + s))
+    
     val vmDeathRequest = virtualMachine.eventRequestManager.createVMDeathRequest()
     val vmThreadStartRequest = virtualMachine.eventRequestManager.createThreadStartRequest()
     val vmThreadDeathRequest = virtualMachine.eventRequestManager.createThreadDeathRequest()
-    var vmThreads: Map[ThreadReference, StepRequest] = Map.empty
+    var vmThreads: Map[ThreadReference, String] = Map.empty
 
     private var _stable: Boolean = false
+
+    private var _monitor: Option[Monitor] = None
 
     def started: Boolean = _monitor.isDefined
     def stable: Boolean = _stable
 
-    def start(): Unit = _monitor match {
-        case None => {
-            log.info("Starting VM \"" + label + "\" with parameters: " + parameters)
-            virtualMachine.setDebugTraceMode(VirtualMachine.TRACE_ALL)
-            vmThreadStartRequest.enable()
-            vmThreadDeathRequest.enable()
-            vmDeathRequest.enable()
-            _monitor = Some(new Monitor(label + " Monitor"))
-        }
-        
-        case Some(_) => sys.error(label + ": virtual machine already started")
-    }
-
-    def stop(): Unit = _monitor match {
-        case Some(th) => {
-            _monitor = None
-            th.interrupt()
-            log.info("Stopping VM \"" + label + "\" with parameters: " + parameters)
-        }
-
-        case None => sys.error(label + ": virtual machine already stopped")
-    }
-
-    def waitForDeath(): Unit = _monitor match {
-        case None => {}
-        case Some(th) => {
-            synchronized { wait() }
-            waitForDeath()
+    def start(): Unit = synchronized {
+        _monitor match {
+            case None => {
+                log.info("Starting VM")
+                //virtualMachine.setDebugTraceMode(VirtualMachine.TRACE_ALL)
+                vmThreadStartRequest.enable()
+                vmThreadDeathRequest.enable()
+                vmDeathRequest.enable()
+                _monitor = Some(new Monitor)
+                _monitor.get.start()
+                stdoutStreamMonitor.start()
+                stderrStreamMonitor.start()
+                notifyAll()
+            }
+            
+            case Some(_) => sys.error("VM already started")
         }
     }
 
-    private val stdoutBuffer = new StringWriter()
-    private val stderrBuffer = new StringWriter()
-    private val byteBuffer = ByteBuffer.allocate(1024)
-    private val chars = Array.ofDim[Char](1024)
-    private val charBuffer = CharBuffer.wrap(chars)
-
-    private val stdoutCharsetDecoder = Charset.defaultCharset.newDecoder()
-    private val stderrCharsetDecoder = Charset.defaultCharset.newDecoder()
-    
-    protected[agent] def acceptInput(which: ProcessStream.Value, channel: SelectableChannel) = {
-        val (buffer, decoder, output) = which match {
-            case ProcessStream.STDOUT => (stdoutBuffer, stdoutCharsetDecoder, (s: String) => log.info(label + ": " + s));
-            case ProcessStream.STDERR => (stderrBuffer, stderrCharsetDecoder, (s: String) => log.error(label + ": " + s));
-        }
-
-        channel match {
-            case (rbc: ReadableByteChannel) => {
-                def processInput(): Unit = {
-                    byteBuffer.clear()
-                    charBuffer.clear()
-                    rbc.read(byteBuffer) match {
-                        case -1 => {
-                            decoder.decode(byteBuffer, charBuffer, true)
-                            decoder.flush(charBuffer)
-                            buffer.write(chars, 0, charBuffer.remaining)
-                        }
-
-                        case n => {
-                            byteBuffer.rewind()
-                            decoder.decode(byteBuffer, charBuffer, false)
-                            buffer.write(chars, 0, charBuffer.remaining)
-                            processInput()
-                        }
-                    }
-                }
-                processInput()
-
-                def readLines(from: Int): Unit =
-                    buffer.getBuffer.indexOf("\n", from) match {
-                        case -1 => buffer.getBuffer.delete(0, from)
-                        case n if n >= 0 => {
-                            output(buffer.getBuffer.substring(0, n))
-                            readLines(from + 1)
-                        }
-                    }
-                readLines(0)
+    def stop(): Unit = synchronized {
+        _monitor match {
+            case Some(th) => {
+                _monitor = None
+                notifyAll()
+                th.interrupt()
+                log.info("Stopping VM with parameters: " + parameters)
             }
 
-            case other =>
-                log.error("Unknown channel subclass with " + other)
+            case None => sys.error("VM already stopped")
         }
     }
 
-    private class Monitor(name: String) extends Thread(name) {
-        override def run(): Unit = loop(true)
+    def waitForDeath(): Unit = synchronized {
+        _monitor match {
+            case None => log.debug("VM finished")
+            case Some(th) => {
+                log.debug("Waiting for VM to finish")
+                synchronized { wait() }
+                waitForDeath()
+            }
+        }
+    }
 
-        private def loop(first: Boolean): Unit =
+    private def clearMonitor(): Unit = synchronized {
+        log.debug("clearing monitor")
+        _monitor = None
+        notifyAll()
+        try { stdoutStreamMonitor.interrupt() } catch { case ex => log.debug("couldn't interrupt stdout:", ex) }
+        try { stderrStreamMonitor.interrupt() } catch { case ex => log.debug("couldn't interrupt stderr:", ex) }
+    }
+
+    private class Monitor extends Thread(label + "-Monitor") {
+        override def run(): Unit = {
+            log.debug("Monitor starting")
+            while (ChildVM.this.synchronized(_monitor).isDefined) {
+                try {
+                    val events = 
+                        if (!_stable) virtualMachine.eventQueue.remove // pend until any event while waiting for VM stability
+                        else virtualMachine.eventQueue.remove(supervisor.quantum)
+
+                    for (event <- if (events != null) events.asScala else Nil) {
+                        event match {
+                            case ThreadStart(vmThread) => {
+                                val name = vmThread.name
+                                log.info("VM thread starting: " + name)
+                                vmThreads += vmThread -> name
+                            }
+
+                            case ThreadDeath(vmThread) => {
+                                val name = vmThreads.get(vmThread) getOrElse "unknown"
+                                log.info("VM thread dying: " + name)
+                                vmThreads -= vmThread
+                            }
+
+                            case VMDeath() => {
+                                log.info("VM dying - monitor exiting")
+                                clearMonitor()
+                                return
+                            }
+
+                            case VMStart(vmThread) => {
+                                log.info("VM started with initial thread: " + vmThread.name)
+                                _stable = true
+                            }
+
+                            case _ => {}
+                        }
+
+                        supervisor.converge()
+
+                        for ((vmThread, name) <- vmThreads if vmThread.isSuspended) {
+                            log.debug("Thread " + name + " is suspended - resuming it")
+                            vmThread.resume()
+                        }
+
+                        log.debug("Resuming the virtual machine")
+                        virtualMachine.resume()
+
+                    }
+                } catch {
+                    case (ie: InterruptedException) => {}
+                    case (ex: Exception) => {
+                        log.error("Monitor dying: ", ex)
+                        clearMonitor()
+                        return
+                    }
+                }
+            }
+            log.debug("Monitor exiting")
+            clearMonitor()
+        }
+    }
+
+    private class StreamLogger(name: String, inputStream: InputStream, output: String => Unit) extends Thread(label + " " + name) {
+        setDaemon(true)
+
+        private val input = new BufferedReader(new InputStreamReader(inputStream))
+
+        override def run(): Unit =
             try {
-                if (!_monitor.isDefined) return
-
-                val events = if (first) virtualMachine.eventQueue.remove(10) else virtualMachine.eventQueue.remove
-                
-                for (event <- events.asScala) {
-                    event match {
-                        case Step(vmThread, location) => {
-                            log.debug(name + ": Step finished for " + vmThread.name + " at " +
-                                      location.sourceName + ":" + location.lineNumber)
-                        }
-
-                        case ThreadStart(vmThread) => {
-                            log.info(name + ": Thread start " + vmThread.name)
-                            val stepRequest = virtualMachine.eventRequestManager.createStepRequest (
-                                vmThread, StepRequest.STEP_MIN, StepRequest.STEP_INTO
-                            )
-                            stepRequest.enable()
-                            vmThreads += ((vmThread, stepRequest))
-                        }
-
-                        case ThreadDeath(vmThread) => {
-                            log.info(name + ": Thread death " + vmThread.name)
-                            vmThreads -= vmThread
-                        }
-
-                        case VMDeath() => {
-                            log.info(name + ": VM Died.")
-                            _monitor = None
-                            return
-                        }
-
-                        case VMStart(vmThread) => {
-                            log.info(name + ": VM Started - initial thread " + vmThread.name)
-                            _stable = true
-                        }
-                    }
-
-                    supervisor.converge()
-
-                    for ((vmThread, _) <- vmThreads if vmThread.isSuspended) {
-                        log.debug(name + ": Thread " + vmThread.name + " is suspended - resuming it")
-                        vmThread.resume()
-                    }
-
-                    log.debug(name + ": Resuming the virtual machine")
-                    virtualMachine.resume()
-
+                while (synchronized(_monitor).isDefined) {
+                    val line = try { input.readLine() } catch { case (_: InterruptedException) => null }
+                    if (line == null) return
+                    else output(line)
                 }
-                    
-                loop(false)
             } catch {
-                case (ie: InterruptedException) => {}
-                case (ex: Exception) => 
-                    log.error(name + ": Monitor died: ", ex)
-
+                case ex => log.info("Died:", ex)
+            } finally {
+                log.debug("Finished.")
             }
     }
-
-    private var _monitor: Option[Monitor] = None
 }
--- agent/src/main/scala/rwars2012/agent/Test.scala
+++ agent/src/main/scala/rwars2012/agent/Test.scala
@@ -1,17 +1,23 @@
 package rwars2012.agent
 
 import net.lag.configgy.Configgy
+import org.slf4j.LoggerFactory
 
 object Test extends App {
+    val log = LoggerFactory.getLogger(getClass)
+
     Configgy.configure(args(0))
 
-    val harnesses = List (
-        new Harness("first",  VMParameters("foo", Set.empty, 10, 1024*1024, 256*1024)),
-        new Harness("second", VMParameters("bar", Set.empty, 20, 1024*1024, 256*1024)),
-        new Harness("third",  VMParameters("baz", Set.empty, 30, 1024*1024, 256*1024))
+    val supervisor = new ChildVMSupervisor(10)
+    val childvms = List (
+        supervisor.add("first",  VMParameters("foo", Set.empty, 10, 1024*1024, 256*1024)),
+        supervisor.add("second", VMParameters("bar", Set.empty, 20, 1024*1024, 256*1024)),
+        supervisor.add("third",  VMParameters("baz", Set.empty, 30, 1024*1024, 256*1024))
     )
 
-    harnesses.foreach(_.start())
+    childvms.foreach(_.start())
 
-    harnesses.foreach(_.waitForDeath())
+    childvms.foreach(_.waitForDeath())
+
+    log.info("Bye.")
 }
